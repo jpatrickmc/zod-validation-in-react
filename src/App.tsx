06a422a7f9bdf70ee1e86cd53e0ebd3953f903a8
Patrick McDaniel <dfwjpm@outlook.com>
2025-07-25 12:22:59 -0500

    add schema; add validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { email, z } from 'zod';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const userSchema = z.object({
+  firstName: z.string(),
+  email: email(),
+  profileUrl: z.url().optional(),
+  age: z.number().min(1).max(120).optional(),
+  friends: z.array(z.string()).optional(),
+  settings: z.object({
+    isSubscribed: z.boolean().default(false),
+  }),
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const userData = {
+  firstName: 'Patrick',
+  email: 'patrick@example.com',
+  profileUrl: 'https://example.com/profile',
+  age: 98,
+  friends: ['Alice', 'Bob'],
+  settings: {
+    isSubscribed: true,
+  },
+};
+
+// Actually use the schema for validation
+// Validate userData using safeParse
+const userResult = userSchema.safeParse(userData);
+
+if (userResult.success) {
+  // userResult.data is of type User
+  console.log(userResult.data);
+} else {
+  // Handle validation errors
+  console.error(userResult.error);
 }
 
-export default App
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Vite + React</h1>
+    </div>
+  );
+}
